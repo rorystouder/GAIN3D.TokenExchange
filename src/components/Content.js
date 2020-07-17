903aea6a27073f4b624aa53f613fe93ccd161aa8
rorystouder <rory.stouder@gmail.com>
2020-07-17 15:13:07 -0400

    Order Book Commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { loadAllOrders } from '../store/interactions'
 import { exchangeSelector } from '../store/selectors'
+import OrderBook from './OrderBook'
 import Trades from './Trades'
+
 
 class Content extends Component {
   componentDidMount() {
@@ -36,17 +38,7 @@ class Content extends Component {
             </div>
           </div>
         </div>
-        <div className="vertical">
-          <div className="card bg-dark text-white">
-            <div className="card-header">
-              Card Title
-            </div>
-            <div className="card-body">
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="/#" className="card-link">Card link</a>
-            </div>
-          </div>
-        </div>
+        <OrderBook />
         <div className="vertical-split">
           <div className="card bg-dark text-white">
             <div className="card-header">
@@ -67,9 +59,8 @@ class Content extends Component {
             </div>
           </div>
         </div>
-        <div className="vertical">
-          <Trades />
-        </div>
+        <Trades />
+
       </div>
     )
   }
